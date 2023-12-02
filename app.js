31b3603e5c25064448d8811aa3f4879b1b6b9235
m.said atabay <matabay200069@gmail.com>
2023-12-02 18:27:08 +0300

    commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const addBtn = document.getElementById("todo-button");
+const todoInput = document.getElementById("todo-input");
+const todoUl = document.getElementById("todo-ul");
+
+addBtn.addEventListener("click", ()=> {
+      if(todoInput.value.trim()===''){ //BOSLUKLARI TRIMLEYIP INPUTU KONTROL ET.
+            alert("Please enter to do");
+      }else{
+            // console.log(todoInput.value.trim());
+            const newTodo = {
+                  id: new Date().getTime(), //MILISANIYE CINSINDEN
+                  text: todoInput.value, //KUTUCUGA GIRILEN MADDE
+                  done: false //GOREV YAPILDI MI                
+            }
+            createListElement(newTodo);
+            //YENI BIR 'LI' ELEMNTI OLUSTURUP BUNU DOM'A BAS.
+            todoInput.value="";
+      }
+
+});
+
+const createListElement = (newTodo) => {
+      const li = document.createElement("li");
+      // li.id = newTodo.id;
+      li.setAttribute("id",newTodo.id); 
+      // 'Lİ' İÇİNE İD DEGİSKENI OLUSTURUP, DEGERINI NEWTODO.ID'SINDEN AL.
+
+      const okIcon = document.createElement("i");
+      okIcon.setAttribute("class", "fa-solid fa-check fa-xl");
+      okIcon.setAttribute("style", "color: #0dc200");
+      li.appendChild(okIcon);
+      //CHECK IKONUNU OLUSTUR, OZELLEŞTIR, 'LI' YE EKLE.
+      
+      const p=document.createElement("p");
+      const pTextNode = document.createTextNode(newTodo.text);
+      p.appendChild(pTextNode);
+      li.appendChild(p);
+      //P ELEMNTI OLUSTUR, ICINE TEXT EKLEYIP 'LI'YE EKLE.
+      
+      const delIcon = document.createElement("i");
+      delIcon.setAttribute("class", "fa-solid fa-trash-can");
+      delIcon.setAttribute("style","color:#e50606");
+      li.appendChild(delIcon);
+      //DELETE IKONUNU OLUSTUR, OZELLEŞTIR, 'LI' YE EKLE.
+      
+      todoUl.appendChild(li);
+      console.log(li);
+}
